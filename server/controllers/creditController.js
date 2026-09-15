c4567b837543fc21f01daafc2be107e15f9af29f
Rohit <rohitpatel72751513@gmail.com>
2026-09-15 23:58:36 +0530

    sec

--- server/controllers/creditController.js
+++ server/controllers/creditController.js
@@ -81,7 +81,7 @@ export const purchasePlan = async (req, res) => {
   mode: 'payment',
   success_url: `${origin}/loading`,
   cancel_url: `${origin}`,
-  metadata: {transcationId: transcation._id.toString(), appId: 'quickgpt'},
+  metadata: {transactionId: transcation._id.toString(), appId: 'quickgpt'},
   expires_at: Math.floor(Date.now()/1000)+30*60, // 30 min
 
 });
